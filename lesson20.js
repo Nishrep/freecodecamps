4cca25352354c01626cd1cf05c87930d45a3e94a
Anton Pershin <pershin.web@gmail.com>
2021-10-10 15:05:27 +0300

    lesson 28

--- lesson20.js
+++ lesson20.js
@@ -1,8 +1,8 @@
-var a = 5;
-var b = 12;
-var c = 4.6;
+var a = 5
+var b = 12
+var c = 4.6
 
 // Only change code below this line
-a = a *= 5;
-b = b *= 3;
-c = c *= 10;
+a = a *= 5
+b = b *= 3
+c = c *= 10
